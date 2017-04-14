a7749de539ba1f80526c6fc45df4bb4b6059de7f
Emmanuel Touzery <etouzery@gmail.com>
2017-04-14 12:32:58 +0200

    match the right drop constraint with the right add constraint by looking at the SQL schema.

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,6 +1,6 @@
 {-# LANGUAGE QuasiQuotes, FlexibleContexts, OverloadedStrings, RecordWildCards #-}
 module Lib
-    ( someFunc
+    ( generateConstraintMigrations
     ) where
 import Text.Regex.PCRE.Heavy
 import qualified Data.Text as T
@@ -8,34 +8,73 @@ import Data.Text (Text)
 import Data.Text.IO as IO
 import Data.Monoid
 import Data.List
+import Data.Map (Map)
+import qualified Data.Map as Map
+import System.Environment
+import Data.Maybe
+
+data ConstraintMigrationInfo = ConstraintMigrationInfo
+    { cstntMigTableName :: Text
+    , cstntMigOldConstraintName :: Text
+    , cstntMigNewConstraintName :: Text
+    } deriving Show
+
+formatConstraint :: ConstraintMigrationInfo -> Text
+formatConstraint ConstraintMigrationInfo{..} =
+    "ALTER TABLE " <> cstntMigTableName <> " RENAME CONSTRAINT \"" <> cstntMigOldConstraintName
+    <> "\" to \"" <> cstntMigNewConstraintName <> "\";"
+
+scanToList :: Regex -> Text -> [[Text]]
+scanToList regex cts = sortOn head $ snd <$> scan regex cts
 
 listToPair :: Show a => [a] -> (a,a)
 listToPair [a,b] = (a,b)
-listToPair v@_ = error ("Not a pair => " ++ show v)
+listToPair list = error ("list not a pair: " ++ show list)
 
-data ConstraintInfo = ConstraintInfo
-                      { tableName :: Text
-                      , oldConstraintName :: Text
-                      , newConstraintName :: Text
-                      } deriving Show
+-- return contraint name => spec
+readOldConstraintsSpecs :: FilePath -> IO (Map Text Text)
+readOldConstraintsSpecs schemaFile = do
+    -- must sort so that the last match is kept, the most recent one
+    cts <- T.replace "\n" "" <$> IO.readFile schemaFile
+    let constraints = scanToList [re|ADD CONSTRAINT (?<constraint>\w+) (.+?);|] cts
+    return $ Map.fromList $ listToPair <$> constraints
 
-formatConstraint :: ConstraintInfo -> Text
-formatConstraint ConstraintInfo{..} =
-    "ALTER TABLE " <> tableName <> " RENAME CONSTRAINT \"" <> oldConstraintName
-    <> "\" to \"" <> newConstraintName <> "\";"
+data NewConstraintInfo = NewConstraintInfo
+    { newConstraintTableName :: Text
+    , newConstraintName      :: Text
+    , newConstraintSpec      :: Text
+    }
 
-pairConstraints :: (Text, Text) -> (Text,Text) -> ConstraintInfo
-pairConstraints (t1, c1) (t2, c2)
-    | t1 == t2  = ConstraintInfo t1 c1 c2
-    | otherwise = error ("expected " ++ show t1 ++ " == " ++ show t2)
+readNewConstraintInfo :: [Text] -> NewConstraintInfo
+readNewConstraintInfo [t,n,s] = NewConstraintInfo t n s
+readNewConstraintInfo sth = error $ "readNewConstraintInfo unexpected format " ++ show sth
 
-someFunc :: IO ()
-someFunc = do
+unsafeMapLookup :: (Ord k, Show k) => String -> Map k a -> k -> a
+unsafeMapLookup msg map_ key = Map.findWithDefault (error $ msg <> " " <> show key) key map_
+
+getConstraintMigrationInfo :: Map Text [Text] -> Map Text Text -> NewConstraintInfo -> ConstraintMigrationInfo
+getConstraintMigrationInfo tableNameToOldConstraints oldConstraintsSpecs NewConstraintInfo{..} =
+    let
+        candidateOldConstraintNames =
+            unsafeMapLookup ("can't find old constraints for " <> show newConstraintName <> " for table") tableNameToOldConstraints newConstraintTableName
+        specMatches = (==) newConstraintSpec
+        oldConstraintName =
+            fromMaybe (error $ "can't find exact old constraint " ++ show newConstraintTableName ++  " "
+                        ++ show newConstraintName ++ " " ++ show candidateOldConstraintNames
+                        ++ "\n" ++ show (unsafeMapLookup "can't find old constraint spec" oldConstraintsSpecs <$> candidateOldConstraintNames)
+                        ++ "\n" ++ show newConstraintSpec) $
+            find (specMatches . unsafeMapLookup "can't find old constraint spec" oldConstraintsSpecs) candidateOldConstraintNames
+    in
+      ConstraintMigrationInfo newConstraintTableName oldConstraintName newConstraintName
+
+generateConstraintMigrations :: IO ()
+generateConstraintMigrations = do
+    oldConstraintsSpecs <- readOldConstraintsSpecs =<< head <$> getArgs
     cts <- T.replace "\n" "" <$> IO.readFile "constraints.txt"
-    let scanToMap regex = sortOn fst $ listToPair . snd <$> scan regex cts
-    let tableToOldConstraint =
-            scanToMap [re|ALTER TABLE (?<table>\w+).+?DROP CONSTRAINT (?<constraint>\w+);|]
-    let tableToNewConstraint =
-            scanToMap [re|ALTER TABLE (?<table>\w+).+?ADD CONSTRAINT (?<constraint>\w+).+?;|]
-    let constraintInfos = zipWith pairConstraints tableToOldConstraint tableToNewConstraint
-    IO.putStrLn $ T.intercalate "\n" $ formatConstraint <$> constraintInfos
+    let tableToOldConstraint = Map.fromListWith (++) $ fmap (replicate 1) . listToPair <$>
+            scanToList [re|ALTER TABLE (?<table>\w+).+?DROP CONSTRAINT (?<constraint>\w+);|] cts
+    let tableToNewConstraint = readNewConstraintInfo <$>
+            scanToList [re|ALTER TABLE (?:ONLY)?\s*(?<table>\w+).+?ADD CONSTRAINT (?<constraint>\w+) (.+?);|] cts
+    let constraintMigrationInfos =
+            getConstraintMigrationInfo tableToOldConstraint oldConstraintsSpecs <$> tableToNewConstraint
+    IO.putStrLn $ T.intercalate "\n" $ formatConstraint <$> constraintMigrationInfos
